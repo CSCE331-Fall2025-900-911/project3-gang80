eb1780a145bf2cece53b7fa0d7e72d963c5acfd1
tristanaujong <tristanaujong@gmail.com>
2025-11-10 13:28:57 -0600

    doioioing

--- client/src/components/Popup.tsx
+++ client/src/components/Popup.tsx
@@ -19,14 +19,14 @@ function Popup({ onClose, title }: PopupProps) {
 
         <div className="flex gap-4">
           <div className="flex flex-1 flex-col bg-gray-50 rounded p-4 justify-center items-center">
-            <h2>{title}</h2>
+            <h2 className="text-3xl font-medium my-20">{title}</h2>
             <div className="max-w-[200px] max-h-[200px] object-contain">
                 <DrinkImage drink="tao"/>
             </div>
           </div>
 
-          <div className="flex flex-1 bg-blue-500 rounded p-4">
-            <p className="text-red-50">Right section content here</p>
+          <div className="flex flex-1 bg-gray-50 rounded p-4 justify-center">
+            <h2 className="text-lg font-medium">Ice Level</h2>
           </div>
         </div>
         
