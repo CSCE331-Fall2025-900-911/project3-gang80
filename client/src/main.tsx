05b26302482054da660852223f8c05501b8bfdef
Larissa Kim <kimLarissa04@gmail.com>
2025-11-24 13:20:10 -0600

    main,tsx fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App'
 import { TranslationProvider } from './contexts/TranslationContext'
+import { ContrastModeProvider } from './contexts/ContrastModeContext';
 
 createRoot(document.getElementById('root')!).render(
+    <ContrastModeProvider>
         <TranslationProvider>
             <App />
         </TranslationProvider>
+    </ContrastModeProvider>
 )
